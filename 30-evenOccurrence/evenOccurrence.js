a5398b4948d36a54d9006f0227f21292da6450dc
rudenick0309 <rudenick0309@gmail.com>
2020-06-29 09:45:45 +0900

    toy30

--- 30-evenOccurrence/evenOccurrence.js
+++ 30-evenOccurrence/evenOccurrence.js
@@ -12,4 +12,26 @@
 
 var evenOccurrence = function(arr) {
   // Your code here.
+  console.log(arr);
+  let arrToObject = arr.reduce((a, c) => {
+    if (a[c]) {
+      a[c]++;
+      return a;
+    } else {
+      a[c] = 1;
+      return a;
+    }
+  }, {});
+  console.log("객체", arrToObject);
+
+  for (let key in arrToObject) {
+    if (arrToObject[key] % 2 === 0) {
+      console.log(typeof key);
+      if (typeof Number(key)) {
+        return key;
+      } else {
+        return Number(key);
+      }
+    }
+  }
 };
