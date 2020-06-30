22139f04a397e44fbe641aaf9f0ea4e164d07d62
rudenick0309 <rudenick0309@gmail.com>
2020-06-30 09:43:12 +0900

    toy31

--- 31-primeTester/primeTester.js
+++ 31-primeTester/primeTester.js
@@ -4,12 +4,19 @@
  * a prime number, false if it's not.
  */
 
-var primeTester = function(n) {
-  if(typeof n !== 'number' || n < 1 || n % 1 !== 0){
+var primeTester = function (n) {
+  // console.log(3111111111125)
+  if (n === 1) return false;
+  if (n === 2) return true;
+  if (typeof n !== "number" || n < 1 || n % 1 !== 0) {
     // n isn't a number or n is less than 1 or n is not an integer
     return false;
+  } else {
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) return false;
+    }
+    return true;
   }
-  // TODO: return true if n is prime, false otherwise
 };
 
 /* Extra credit: Write a function that generates a list of all prime numbers
@@ -17,8 +24,10 @@ var primeTester = function(n) {
  * check out the Sieve of Eratosthenes on Wikipedia. (And if you're feeling
  * saucy, check out the Sieve of Atkin.)
  */
-
 var primeSieve = function (start, end) {
+  let temp = [];
+  for (let i = start; i <= end; i++) {
+    if (primeTester(i)) temp.push(i);
+  }
+  return temp;
 };
-
-
