2e32b596160172f83e73f975b6cf43a7338c9c4b
rudenick0309 <rudenick0309@gmail.com>
2020-07-16 09:36:42 +0900

    'toy43'

--- 43-threeFiveMultiples/threeFiveMultiple.js
+++ 43-threeFiveMultiples/threeFiveMultiple.js
@@ -7,4 +7,23 @@
 
 function threeFiveMultiples(num) {
   // 여기에 코드를 작성하세요
+
+  let temp = new Array(num - 1);
+  // num의 길이만큼 배열을 1부터 만든다.
+  for (let i = 0; i < num - 1; i++) {
+    temp[i] = i + 1;
+  }
+
+  // 필터 메소드를 사용해서 조건을, 3으로 나누어 떨어지는 경우와 '혹은' 5로 나누어 떨어지는 경우를 설정한다.
+  let filttered = temp.filter((v) => {
+    return v % 3 === 0 || v % 5 === 0
+  })
+
+  // 리듀스를 사용한다.
+  let reduced = filttered.reduce((a,c) => {
+    return a + c;
+  })
+
+  // 아니면 처음부터 리듀스를 사용해서, 그 안에서 조건을 건다.
+  return reduced;
 }
